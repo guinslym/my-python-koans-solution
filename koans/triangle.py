a47bb40e47c0dfe4f0fc865447baaff4ac035947
Guinslym <guinslym@gmail.com>
2016-11-18 21:12:24 -0500

    about triangles project

--- koans/triangle.py
+++ koans/triangle.py
@@ -17,8 +17,13 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    side1, side2, side3 = a, b, c
+    list_side = [side1, side2, side3]
+    if list_side.count(side1) == 3:
+    	return 'equilateral'
+    if list_side.count(side1) == 2 or list_side.count(side3) == 2:
+    	return 'isosceles'
+    return 'scalene'
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
