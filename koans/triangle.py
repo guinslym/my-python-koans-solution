cd17b99b70cc6d01de999679410f1f96d4b4c930
Guinslym <guinslym@gmail.com>
2016-11-18 21:34:19 -0500

    about triangle 12

--- koans/triangle.py
+++ koans/triangle.py
@@ -16,9 +16,25 @@
 # and
 #   about_triangle_project_2.py
 #
+
+class TriangleError(RuntimeError):
+    pass  
+
 def triangle(a, b, c):
     side1, side2, side3 = a, b, c
     list_side = [side1, side2, side3]
+
+    # part 2
+    f = lambda x: x < 0
+    negatif_list = [f(x) for x in list_side]
+    if 0 in list_side:
+    	raise(TriangleError)
+    elif True in negatif_list:
+    	raise(TriangleError)
+    if list_side.count(1) == 2 or list_side.count(2) == 2:
+    	raise(TriangleError)
+
+    # part 1
     if list_side.count(side1) == 3:
     	return 'equilateral'
     if list_side.count(side1) == 2 or list_side.count(side3) == 2:
