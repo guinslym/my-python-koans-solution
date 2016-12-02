1e42719c86ef922799c71053fbc40e893fa8f65f
Guinslym <guinslym@gmail.com>
2016-12-02 04:01:17 -0500

    17

--- koans/about_comprehension.py
+++ koans/about_comprehension.py
@@ -14,7 +14,7 @@ class AboutComprehension(Koan):
         comprehension = [delicacy.capitalize() for delicacy in feast]
 
         self.assertEqual('Lambs', comprehension[0])
-        self.assertEqual(__, comprehension[2])
+        self.assertEqual('Orangutans', comprehension[2])
 
     def test_filtering_lists_with_list_comprehensions(self):
         feast = ['spam', 'sloths', 'orangutans', 'breakfast cereals',
