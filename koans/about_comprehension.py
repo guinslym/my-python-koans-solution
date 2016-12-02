26ea6f1956209d24530f09f560dfb9ffa3281519
Guinslym <guinslym@gmail.com>
2016-12-02 04:05:27 -0500

    43

--- koans/about_comprehension.py
+++ koans/about_comprehension.py
@@ -40,7 +40,7 @@ class AboutComprehension(Koan):
         comprehension = [ '{0} and {1}'.format(egg, meat) for egg in list_of_eggs for meat in list_of_meats]
 
 
-        self.assertEqual(__, comprehension[0])
+        self.assertEqual('poached egg and lite spam', comprehension[0])
         self.assertEqual(__, len(comprehension))
 
     def test_creating_a_set_with_set_comprehension(self):
