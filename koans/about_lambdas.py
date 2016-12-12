6abaa657e7cdc6318283735d59c18c52045aec9e
Guinslym <guinslym@gmail.com>
2016-12-12 14:32:22 -0500

    13

--- koans/about_lambdas.py
+++ koans/about_lambdas.py
@@ -10,7 +10,7 @@ from runner.koan import *
 class AboutLambdas(Koan):
     def test_lambdas_can_be_assigned_to_variables_and_called_explicitly(self):
         add_one = lambda n: n + 1
-        self.assertEqual(__, add_one(10))
+        self.assertEqual(11, add_one(10))
 
     # ------------------------------------------------------------------
 
