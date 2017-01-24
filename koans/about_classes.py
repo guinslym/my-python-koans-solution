6ba254e56df8bc5ef51ee4c56238196e6280fa35
Guinsly Mondésir <guinslym@gmail.com>
2017-01-24 03:37:44 -0500

    dog

--- koans/about_classes.py
+++ koans/about_classes.py
@@ -28,7 +28,7 @@ class AboutClasses(Koan):
 
     def test_init_method_is_the_constructor(self):
         dog = self.Dog2()
-        self.assertEqual(__, dog._name)
+        self.assertEqual('Paul', dog._name)
 
     def test_private_attributes_are_not_really_private(self):
         dog = self.Dog2()
