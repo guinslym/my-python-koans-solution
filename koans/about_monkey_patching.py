025b24505b3d0bf233b65ad358f250ba45a90993
Guinsly Mondésir <guinslym@gmail.com>
2017-01-24 04:38:27 -0500

    monkey_patching

--- koans/about_monkey_patching.py
+++ koans/about_monkey_patching.py
@@ -24,8 +24,8 @@ class AboutMonkeyPatching(Koan):
         self.Dog.wag = wag
 
         fido = self.Dog()
-        self.assertEqual(__, fido.wag())
-        self.assertEqual(__, fido.bark())
+        self.assertEqual('HAPPY', fido.wag())
+        self.assertEqual("WOOF", fido.bark())
 
     # ------------------------------------------------------------------
 
